b4ffefb8084d48fff1932610d9fada3160a0eafa
keyi.feng@ubtrobot.com <keyi.feng@ubtrobot.com>
2019-07-23 16:06:50 +0800

    拍照APP实现

--- lib/photo_take_page.dart
+++ lib/photo_take_page.dart
@@ -14,14 +14,15 @@ class TakePhotoPage extends StatefulWidget {
 
 class _TakePhotoPageState extends State<TakePhotoPage> {
 
-  File _image;
+  List _images = [];
 
-  Future getImage() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+  Future getImage(bool isTakePhoto) async {
+    var image = await ImagePicker.pickImage(source: isTakePhoto?ImageSource.camera:ImageSource.gallery);
     bool isImageNull = image==null;
+    Navigator.pop(context);
     setState(() {
       print('image is null? $isImageNull');
-      _image = image;
+      _images.add(image);
     });
   }
 
@@ -32,22 +33,75 @@ class _TakePhotoPageState extends State<TakePhotoPage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _image==null?Text('not image to show.'):
-             Image.file(_image,width: 100,height: 100,)
-          ],
-        ),
+        child: Wrap(
+          spacing: 5,
+          runSpacing: 5,
+          children:_showImages()
+        )
       ),
-      floatingActionButton: FloatingActionButton(onPressed: getImage,
+      floatingActionButton: FloatingActionButton(onPressed: _showImgeSelect,
       child: Icon(Icons.add_a_photo,size: 50,),
       ),
     );
   }
 
-  _selectDialog()
-  {
 
+   _showImgeSelect() {
+    showModalBottomSheet(context: context, builder: (context)=>
+    Container(
+      height: 160,
+      child: Column(
+        children: <Widget>[
+          _item("拍照",true),
+          _item("相册",false),
+        ],
+      ),
+    ));
   }
+
+  _item(String title, bool isTakePhoto) {
+      return GestureDetector(
+        onTap:()=>getImage(isTakePhoto),
+        child: ListTile(
+          leading: Icon(isTakePhoto?Icons.camera_alt:Icons.photo_library),
+            title: Text(title),
+        ),
+      );
+  }
+
+  _showImages() {
+    return _images.map((file){
+      return Stack(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.file(file,width: 120,height: 90,fit: BoxFit.fill,),
+          ),
+          Positioned(
+            right:5,
+            top: 5,
+          child: GestureDetector(
+            onTap: (){
+              setState(() {
+                  _images.remove(file);
+              });
+            },
+            child: ClipOval(
+           child: Container(
+               padding: EdgeInsets.all(5),
+               decoration: BoxDecoration(color: Colors.black54),
+               child: Icon(Icons.close,color: Colors.white,size: 18,),
+           ),
+          ),
+          ),
+          ),
+        ],
+      );
+    }).toList();
+  }
+
 }
+
+
+
+
