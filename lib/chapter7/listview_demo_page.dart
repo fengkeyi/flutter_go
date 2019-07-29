132459ba0a5746f340a22edc58946fb508527a72
fengkeyi <winfengkeyi@outlook.com>
2019-07-29 23:07:50 +0800

    列表下拉刷新上拉加载更多数据RefreshIndcator and ScrollerController

--- lib/chapter7/listview_demo_page.dart
+++ lib/chapter7/listview_demo_page.dart
@@ -10,32 +10,67 @@ class ListViewDemo extends StatefulWidget {
 }
 
 class _ListViewDemoState extends State<ListViewDemo> {
+  ScrollController _controller;
   CityModule _cityModule;
-  final _cityUrl = 'http://zhouxunwang.cn/data/?id=104&key=UOCU8YM3S4z+ipiB9IMzQ2rJMwTgsJeZ/pxz7fk&ske=1';
+  final _cityUrl =
+      'http://zhouxunwang.cn/data/?id=104&key=UOCU8YM3S4z+ipiB9IMzQ2rJMwTgsJeZ/pxz7fk&ske=1';
+
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  void initState() {
+    _controller = ScrollController();
+    _controller.addListener((){
+      if(_controller.position.pixels==_controller.position.maxScrollExtent) {
+        _loadMoreData();
+      }
+    });
+    super.initState();
+  }
 
   List<Widget> _buildItems() {
-    return _cityModule.result.map((city){
+    return _cityModule.result.map((city) {
       return _buildItem(city);
     }).toList();
   }
-  
-  Widget _buildItem(Result result){
+
+  Widget _buildItem(Result result) {
     return Container(
       color: Colors.teal,
       alignment: Alignment.center,
       margin: EdgeInsets.only(bottom: 5),
       padding: EdgeInsets.all(10),
-      child: Text(result.province,style: TextStyle(fontSize: 25,color: Colors.grey),),
+      child: Text(
+        result.province,
+        style: TextStyle(fontSize: 25, color: Colors.grey),
+      ),
     );
   }
 
-  void _loadDatas() async{
-      var response = await http.get(_cityUrl);
-      Utf8Codec utf8codec = Utf8Codec();
-      var result = json.decode(utf8codec.decode(response.bodyBytes));
-      setState(() {
-        _cityModule = CityModule.fromJson(result);
-      });
+  void _loadDatas() async {
+    var response = await http.get(_cityUrl);
+    Utf8Codec utf8codec = Utf8Codec();
+    var result = json.decode(utf8codec.decode(response.bodyBytes));
+    setState(() {
+      _cityModule = CityModule.fromJson(result);
+    });
+  }
+
+  Future<Null> _loadMoreData() async {
+    var response = await http.get(_cityUrl);
+    Utf8Codec utf8codec = Utf8Codec();
+    var result = json.decode(utf8codec.decode(response.bodyBytes));
+    setState(() {
+      CityModule cityModule = CityModule.fromJson(result);
+      _cityModule.result.addAll(cityModule.result);
+    });
+    return null;
   }
 
   @override
@@ -49,9 +84,12 @@ class _ListViewDemoState extends State<ListViewDemo> {
               value: 50,
               backgroundColor: Colors.deepOrange,
             )
-          : ListView(
-              children: _buildItems(),
-            ),
+          : RefreshIndicator(
+              child: ListView(
+                controller: _controller,
+                children: _buildItems(),
+              ),
+              onRefresh: _loadMoreData),
       floatingActionButton: FloatingActionButton(
         onPressed: _loadDatas,
         backgroundColor: Colors.transparent,
